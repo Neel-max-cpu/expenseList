c4c1b610b2c029cdd91db7d0432702466c7f78dc
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-05-19 16:03:27 +0530

    upload route :memo:

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,12 @@ import dotenv from 'dotenv'
 import {connectDB} from './config/db.js'
 import authRoutes from './routes/auth.routes.js'
 
+
+// for upload --
+import {fileURLToPath} from 'url'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
@@ -19,11 +25,17 @@ app.use(
     })
 );
 
-app.use(express.json());
+// for img too --
+app.use(express.json({ limit: "10mb" }));
 
 connectDB();
 
 app.use("/api/v1/auth", authRoutes);
+
+// server upload folder --- 
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));       // for commonjs works directly
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));           // for module have to declare
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
