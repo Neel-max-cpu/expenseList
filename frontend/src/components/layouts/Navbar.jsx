6834f5328cabf675af57721df0c530379b4ca09c
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-05-21 17:06:41 +0530

    navbar :memo:

---
+++ frontend/src/components/layouts/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import SideMenu from './SideMenu';
+
+// icons --
+import { HiOutlineMenu, HiOutlineX  } from "react-icons/hi";
+
+const Navbar = ({activeMenu}) => {
+    const [openSideMenu, setOpenSideMenu] = useState(false);
+    
+  return (
+    <div className="flex gap-5 bg-[#101212] border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30">
+        <button 
+            className="hover:cursor-pointer block lg:hidden"
+            onClick={()=>{
+                setOpenSideMenu(!openSideMenu);
+            }}
+        >
+            {openSideMenu ? (
+                <HiOutlineX className='text-2xl'/>
+            ) : (
+                <HiOutlineMenu className='text-2xl'/>
+            )}
+        </button>
+
+        <h2 className="text-lg font-medium">Expense Tracker</h2>
+
+        {openSideMenu && (
+            <div className="">
+                <SideMenu activeMenu={activeMenu}/>
+            </div>
+        )}
+    </div>
+  )
+}
+
+export default Navbar
