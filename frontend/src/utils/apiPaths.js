979095a42a15acf942d438d1c848fa9eceb0ad54
Neel Bhattacharya <neelbhatta22@gmail.com>
2025-06-25 23:15:49 +0530

    Update :memo:

--- frontend/src/utils/apiPaths.js
+++ frontend/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5001";
+// export const BASE_URL = "http://localhost:5001";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001"
